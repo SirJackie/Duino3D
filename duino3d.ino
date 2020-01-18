dd516a477f64d41f950ef45fd6c775de6549c9dd
lyj00912 <3122742760@qq.com>
2020-01-18 13:49:04 +0800

    create duino3d.ino and include Duino9341.h

---
+++ duino3d.ino
@@ -0,0 +1,12 @@
+#include "Duino9341.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  LcdInit();
+  LcdFill(0,0,239,319,RGB(255,255,255));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
